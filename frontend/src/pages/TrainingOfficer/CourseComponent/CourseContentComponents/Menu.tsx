02e7cdba9d9cfd1cc1484a576b07eedf710ecd91
AllenKirby <130225133+AllenKirby@users.noreply.github.com>
2025-02-26 05:16:30 +0800

    other course content functionality added

--- frontend/src/pages/TrainingOfficer/CourseComponent/CourseContentComponents/Menu.tsx
+++ frontend/src/pages/TrainingOfficer/CourseComponent/CourseContentComponents/Menu.tsx
@@ -29,16 +29,17 @@ type MenuState = {
   menuID: string;
   addModule: (menuID: string) => void;
   menuData: MenuDataState;
-  setSelectedMenu: (menuID: string) => void
-  deleteModule: (menuID: string, moduleID: string) => void
+  deleteModule: (menuID: string, moduleID: string) => void;
+  selectMenu: (menuID: string) => void;
+  selectModule: (moduleID: string) => void;
 }
 
 const Menu: React.FC<MenuState> = (props) => {
-  const { menuID, addModule, menuData, setSelectedMenu, deleteModule } = props
+  const { menuID, addModule, menuData, deleteModule, selectMenu, selectModule } = props
   const [collapse, setCollapse] = useState<boolean>(false);
 
   return (
-    <section onClick={() => setSelectedMenu(menuID)} className="w-full h-auto rounded-md border cursor-pointer">
+    <section className="w-full h-auto rounded-md border cursor-pointer">
       <div className={`w-full py-1 px-2 flex items-center justify-between bg-c-green-60 ${collapse === false? "rounded-t-md" : "rounded-md "}`}>
         <p className="text-p-rg text-f-light">{menuData.title}</p>
         <button className="px-1"><FiChevronDown size={20} color="white" onClick={() => setCollapse(!collapse)}/></button>
@@ -46,7 +47,7 @@ const Menu: React.FC<MenuState> = (props) => {
       <div className={`px-2 py-4 flex flex-col items-center justify-center ${collapse === false? "block" : "hidden"}`}>
         <div className="w-full">
           {menuData.modules.map((m: ModuleState) => (
-            <div className="w-full flex items-center justify-between mb-2 text-c-grey-50 group">
+            <div onClick={() => {selectMenu(menuID); selectModule(m.moduleID);}} className="w-full flex items-center justify-between mb-2 text-c-grey-50 group">
               <p key={m.moduleID}>{m.title ? m.title : 'Untitled'}</p>
               <button className="hidden group-hover:block" onClick={() => deleteModule(menuID, m.moduleID)}><IoMdClose size={16}/></button>
             </div>
